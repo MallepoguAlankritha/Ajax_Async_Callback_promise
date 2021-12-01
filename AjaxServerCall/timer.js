eeaa8735722b5df38c196590d5590cfe190a4fd1
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-12-01 20:06:03 +0530

    Added:Ability to view Employee data using Ajax Promise

--- AjaxServerCall/timer.js
+++ AjaxServerCall/timer.js
@@ -16,4 +16,3 @@
     console.log("Activity-A: Triggering Activity-B at " +showTime());
     setTimeout(showSessionExpire, 5000);
     console.log("Activity-A: Triggered Activity-B at " +showTime()+ " will execute after 5 seconds.");
-    
